894e84449a8e10e03ba8ed711a2e230b0bc9a7f1
omarsagoo <om.sagoo@gmail.com>
2019-09-17 18:51:15 -0700

    display gifs on page

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template, request
-import requests
 import json
+import unittest
+
+import requests
+from flask import Flask, render_template, request
+
+import app
 
 app = Flask(__name__)
 
@@ -10,7 +14,8 @@ def index():
     """Show the homepage and ask the user's name."""
     
     
-    # TODO: Extract the query term from url using request.args.get()
+    # Extract the query term from url using request.args.get()
+    
     user_search = request.args.get("search")
 
     params = {
@@ -22,23 +27,38 @@ def index():
     # TODO: Make an API call to Tenor using the 'requests' library. For 
     # reference on how to use Tenor, see: 
     # https://tenor.com/gifapi/documentation
-    r = requests.get("https://api.tenor.com/v1/search", params)
+    response = requests.get("https://api.tenor.com/v1/search", params)
 
     # TODO: Use the '.json()' function to get the JSON of the returned response
     # object
-    gif_json = r.json()
+    gif_json = response.json()
 
-    # TODO: Using dictionary notation, get the 'results' field of the JSON,
+    # Using dictionary notation, get the 'results' field of the JSON,
     # which contains the GIFs as a list
-    # for i in range(len(gif_json['results'])):
-    gifs = gif_json['results'] #[i]['media'][0]['gif']['url']
-
-    return render_template('index.html', gifs=gifs)
+    if response.status_code == 200:
+        gif_list = gif_json['results']
+    else:
+        gif_list = None
+   
     # TODO: Render the 'index.html' template, passing the list of gifs as a
     # named parameter called 'gifs'
+    return render_template('index.html', gifs=gif_list)
+
+
+class appTest(unittest.TestCase):
+
+    def setUp(self):
+        # creates a test client
+        self.app = app.test_client()
+        # propagate the exceptions to the test client
+        self.app.testing = True
+
+    def test_index(self):
+        result = self.app.get('/')
+
+        self.assertEqual(result.status_code, 200)
 
 
 if __name__ == '__main__':
+    unittest.main()
     app.run(debug=True)
-
-    
