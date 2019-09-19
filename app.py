e48f7470b1315926a4f87ff9ee572ec27ee9b673
omarsagoo <om.sagoo@gmail.com>
2019-09-18 17:15:35 -0700

    clean up code and add comments

--- app.py
+++ app.py
@@ -8,11 +8,22 @@ import app
 
 app = Flask(__name__)
 
-def trending_gifs():
-    response = requests.get("https://api.tenor.com/v1/trending?", params = {"key": "FBQJ8PNF0RXL", "limit": 9})
 
+def trending_gifs():
+    # stores params in a dict variable for the api respponse
+    params = {
+    "key": "FBQJ8PNF0RXL",
+    "limit": 9
+    }
+
+    #retrieves the API response 
+    response = requests.get("https://api.tenor.com/v1/trending?", params)
+    # stores the api response in json in a dict variable
     gif_json = response.json()
 
+    #Using dictionary notation, get the 'results' field of the JSON,
+    # which contains the GIFs as a list, 
+    # If statement checks to make sure that if the server doesnt have anything to return, it doesnt crash
     if response.status_code == 200:
         gif_list = gif_json['results']
     else:
@@ -21,10 +32,21 @@ def trending_gifs():
     return  render_template('index.html', gifs=gif_list)
 
 def random_gif():
-    response = requests.get("https://api.tenor.com/v1/random?", params = {"key":"FBQJ8PNF0RXL", "q": "random", "limit": 9})
+    # stores params in a dict variable for the api respponse
+    params = {
+        "key":"FBQJ8PNF0RXL",
+        "q": "random",
+        "limit": 9
+    }
 
+    #retrieves the API response 
+    response = requests.get("https://api.tenor.com/v1/random?", params)
+    # stores the api response in json in a dict variable
     gif_json = response.json()
 
+    #Using dictionary notation, get the 'results' field of the JSON,
+    # which contains the GIFs as a list, 
+    # If statement checks to make sure that if the server doesnt have anything to return, it doesnt crash
     if response.status_code == 200:
         gif_list = gif_json['results']
     else:
@@ -33,15 +55,22 @@ def random_gif():
     return  render_template('index.html', gifs=gif_list)
 
 def search_gif(user_search):
+    # stores params in a dict variable for the api respponse
     params = {
     "q": user_search,
     "key": "FBQJ8PNF0RXL",
     "limit": 9
     }
 
+    #retrieves the API response 
     response = requests.get("https://api.tenor.com/v1/search", params)
+     # stores the api response in json in a dict variable
     gif_json = response.json()
 
+    
+    #Using dictionary notation, get the 'results' field of the JSON,
+    # which contains the GIFs as a list, 
+    # If statement checks to make sure that if the server doesnt have anything to return, it doesnt crash
     if response.status_code == 200:
         gif_list = gif_json['results']
     else:
@@ -49,14 +78,15 @@ def search_gif(user_search):
     
     return  render_template('index.html', gifs=gif_list)
 
+
 @app.route('/')
 def index():
     """Show the homepage and ask the user's name."""
     # accepts user input from the html and passes it in to the query param
     user_search = request.args.get("search")
 
-    # stores params in a dict variable for the api respponse
-
+    # if statement that checks if there is any user input, trending, or random gifs. if not then it wont display any gifs. 
+    # also doesnt display any gifs on startup
     if request.args.get("trending") == "trending":
         return trending_gifs()
     elif request.args.get("random") == "random":
@@ -66,17 +96,15 @@ def index():
     else:
         return  render_template('index.html', gifs=list())
 
-    #retrieves the API response 
+    
 
 
-    # stores the api response in json in a dict variable
+   
 
-    # Using dictionary notation, get the 'results' field of the JSON,
-    # which contains the GIFs as a list
     
     
-    # if statement that checks if there is any user input, if not then it wont display any gifs. 
-    # also doesnt display any gifs on startup
+    
+   
     
 
 class appTest(unittest.TestCase):
